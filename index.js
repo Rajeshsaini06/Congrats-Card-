f51c9493b90ec78c5ecc5e226f01dadfa7d8d119
rajesh <rajeshrakee66@gmail.com>
2023-05-27 06:18:35 +0000

    congrats card

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  // Write your code here.
+  <div class="congrats-card-bg-container">
+    <h1 class="heading">Congratulations</h1>
+    <div class="card-container">
+      <img
+        class="image"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+      />
+      <h1 class="name-heading">Kiran V</h1>
+      <p class="paragraph">
+        Vishnu Institute of Computer Education and Technology,Bhimavaram
+      </p>
+      <img
+        class="image"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
